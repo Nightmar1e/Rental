01c5aee003b7669dded5544df2d03d1a1d9176f8
Nightmar1e <walidelghalidi2001@gmail.com>
2023-09-05 13:49:24 +0200

    Update Team.jsx

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -6,17 +6,29 @@ import Person3 from "../images/team/3.png";
 import Person4 from "../images/team/4.png";
 import Person5 from "../images/team/5.png";
 import Person6 from "../images/team/6.png";
+import Person7 from "../images/team/7.png";
+import Person8 from "../images/team/8.png";
+import Person9 from "../images/team/9.png";
+// 363 * 447
 
+{/* 
+  7 -> 1
+  1 -> 7
+  5 -> 6
+  6 -> 5
+
+*/}
 function Team() {
   const teamPpl = [
-    { img: Person1, name: "Luke Miller", job: "Salesman" },
-    { img: Person2, name: "Michael Diaz", job: "Business Owner" },
-    { img: Person3, name: "Briana Ross", job: "Photographer" },
-    { img: Person4, name: "Lauren Rivera", job: "Car Detailist" },
-    { img: Person5, name: "Martin Rizz", job: "Mechanic" },
-    { img: Person6, name: "Caitlyn Hunt", job: "Menager" },
-    { img: Person6, name: "Caitlyn Hunt", job: "Menager" },
-    { img: Person6, name: "Caitlyn Hunt", job: "Menager" },
+    { img: Person1, name: "James Mitchell", job: "Business Owner" },
+    { img: Person2, name: "Benjamin Parker", job: "Manager" },
+    { img: Person3, name: "Sophia Roberts", job: "Photographer" },
+    { img: Person4, name: "Emma Taylor", job: "Maintenance Technician" },
+    { img: Person5, name: "Olivia Clark", job: "Accounting Clerk" },
+    { img: Person6, name: "Ava Foster", job: "Marketing Specialist" },
+    { img: Person7, name: "William Turner", job: "Maintenance Technician" },
+    { img: Person8, name: "Daniel Harris", job: "Maintenance Technician" },
+    { img: Person9, name: "Mia Johnson", job: "Vehicle Inspector" },
   ];
   return (
     <>
